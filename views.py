7956e1a72ccf913b32414a6125dd2427808ab5af
Barneedhar Vigneshwar <barneedhar@gmail.com>
2022-11-26 17:25:48 +0530

    Record assigning data

--- views.py
+++ views.py
@@ -71,9 +71,7 @@ def payment_add_page():
         title = form.data["title"]
         date = form.data["date"]
         amount = form.data["amount"]
-
         mode = form.data["mode"]
-
 
         modeentry = form.data["modeentry"]
         rel_manager = form.data["rel_manager"]
@@ -88,9 +86,13 @@ def payment_add_page():
 
         voucher = form.data["voucher"]
         created_date = datetime.now()
-
         created = created_date.strftime("%d/%m/%Y %H:%M:%S")
-        history = created + ": "+ status
+
+        if underwriter != "":
+            underwriter_history = "<br>"+created + ": Assigned to " + underwriter
+            history = created + ": "+ status +underwriter_history
+        else:
+            history = created + ": "+ status
 
         if status == "Completed":
             completed = created
@@ -119,20 +121,34 @@ def validate_payment_form():
     return len(form.errors) == 0
 
 
+def compare_underwriter(dt_string,old_value, new_value):
+
+    if old_value != new_value:
+        if new_value != "":
+            underwriter_update =  '<br>'+dt_string + ": Assigned to " + new_value
+        else:
+            underwriter_update =  '<br>'+dt_string +": Task has been unassigned."
+        return underwriter_update
+    else:
+        return ""
+
 @login_required
 def payment_edit_page(payment_key):
 
     db = current_app.config["db"]
     payment = db.get_payment(payment_key)
     form = PaymentEditForm()
+
     if form.validate_on_submit():
         title = form.data["title"]
         date = form.data["date"]
         amount = form.data["amount"]
+
         mode = form.data["mode"]
         modeentry = form.data["modeentry"]
         customerid = form.data["customerid"]
         rel_manager = form.data["rel_manager"]
+
         broker = form.data["broker"]
         nature = form.data["nature"]
         remarks = form.data["remarks"]
@@ -141,39 +157,54 @@ def payment_edit_page(payment_key):
         ticket = form.data["ticket"]
         status = form.data["status"]
         voucher = form.data["voucher"]
+
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         update = dt_string +  ': ' +status
+
         if payment.history != None:
             if payment.status != form.data['status']:
-                history = payment.history +' <br/>' +update
+                underwriter_update = compare_underwriter(dt_string, payment.underwriter, underwriter)
+                history = payment.history +'<br>'+update+underwriter_update
+
             else:
-                history = payment.history
+                underwriter_update = compare_underwriter(dt_string, payment.underwriter, underwriter)
+                history = payment.history + underwriter_update
+
         else:
-            history = update
+            underwriter_update = compare_underwriter(dt_string, payment.underwriter, underwriter)
+            history = update + underwriter_update
+
         if status == "Completed":
             completed = dt_string
         else:
             completed = None
+
         payment = Payment(title, date=date, amount=amount, mode = mode,
                 modeentry=modeentry, customerid= customerid, rel_manager=rel_manager, broker=broker,
                 nature=nature,remarks=remarks,underwriter=underwriter,
                 ticket=ticket, status=status, voucher=voucher, history = history, completed = completed)
         db.update_payment(payment_key, payment)
+
         flash("Payment data updated.")
         return redirect(url_for("payment_page", payment_key = payment_key))
+
     form.title.data = payment.customer
+
     form.date.data = datetime.strptime(payment.date, '%Y-%m-%d') if payment.date else ""
     form.amount.data = payment.amount
     form.mode.data = payment.mode
     form.modeentry.data = payment.modeentry
+
     form.customerid.data = payment.customerid
     form.rel_manager.data = payment.rel_manager
     form.broker.data = payment.broker
     form.nature.data = payment.nature
+
     form.remarks.data = payment.remarks
     form.underwriter.data = payment.underwriter
     form.ticket.data = payment.ticket
+
     form.status.data = payment.status
     form.voucher.data = payment.voucher
     form.history.data = payment.history
